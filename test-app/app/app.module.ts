c8d67055916dd01620602cfb810b772cd7c8e706
Newteq <newt.fourie@entelect.co.za>
2019-05-23 15:39:54 +0200

    everything seems to be working in this template example

--- test-app/app/app.module.ts
+++ test-app/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestLibModule } from 'projects/test-lib/src/public-api';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		TestLibModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
